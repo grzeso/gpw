69ba6a764d39662ea7605c8ee66a392a828775e2
Grzesiek <grzeso@interia.pl>
2022-11-20 21:37:26 +0100

    composer - update symfony do 6.1 GPW-43

--- config/bundles.php
+++ config/bundles.php
@@ -9,6 +9,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
